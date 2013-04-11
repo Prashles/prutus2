294a3632a15e8edde1827b73952ded2a6a7f0767
xxOrpheus <lolidunno@live.co.uk>
2013-04-10 21:21:38 -0700

    Implemented hash bruteforcing. Sloppified the code

--- class.prutus.php
+++ class.prutus.php
@@ -10,6 +10,10 @@ class Prutus {
     protected $originalURL = '', $url = array();
     protected $postFields = array();
 
+
+    protected $fingerprints = array();
+    protected $hash = false, $hashType = null, $pattern = '%word%';
+
     /**
      *
      * Constructor.
@@ -35,6 +39,8 @@ class Prutus {
                 $this->setTargetURL($url);
             }
         }
+
+        $this->loadFingerPrints(__DIR__ . '/common/hash_fingerprints.json');
     }
     
     /**
@@ -47,35 +53,16 @@ class Prutus {
      * @return mixed
      *
      */
-    public function start(\closure $parse, $rounds = 0) {
-        $this->cURLHandler = curl_multi_init();
-        $ch = array();
-
-        if(!$this->permutation) {
-            foreach($this->wordList as $word) {
-                $ch[$word] = curl_init($this->originalURL);
-                $postFields = array_map(function($value) use($word) {
-                    return preg_replace('/\%word\%/', $word, $value);
-                }, $this->postFields);
-                curl_setopt_array($ch[$word], array(CURLOPT_RETURNTRANSFER => true, CURLOPT_POSTFIELDS => $postFields));
-                curl_multi_add_handle($this->cURLHandler, $ch[$word]);
+    public function start(\closure $parse = null, $rounds = 0) {
+        if($this->hash === false) {
+            if($parse === null) {
+                throw new \Exception(__METHOD__ . ': $parse was null');
             }
+            $this->cURLHandler = curl_multi_init();
+            $ch = array();
 
-            $running = null;
-            do {
-                curl_multi_exec($this->cURLHandler, $running);
-            } while($running > 0);
-
-            foreach($ch as $password => $handler) {
-                $ch[$password] = $parse(curl_multi_getcontent($handler), $password);
-            }
-        } else {
-            $h = 0;
-            $i = 0;
-            $done = false;
-            while($done == false) {
-                for($j = 0; $j < $this->buffer; $j++) {
-                    $word = base_convert($h, 10, 36);
+            if(!$this->permutation) {
+                foreach($this->wordList as $word) {
                     $ch[$word] = curl_init($this->originalURL);
                     $postFields = array_map(function($value) use($word) {
                         return preg_replace('/\%word\%/', $word, $value);
@@ -91,23 +78,128 @@ class Prutus {
 
                 foreach($ch as $password => $handler) {
                     $ch[$password] = $parse(curl_multi_getcontent($handler), $password);
-                    if($ch[$password]) {
-                        return $password;
+                }
+            } else {
+                $h = 0;
+                $i = 0;
+                $done = false;
+                while($done == false) {
+                    for($j = 0; $j < $this->buffer; $j++) {
+                        $word = base_convert($h, 10, 36);
+                        $ch[$word] = curl_init($this->originalURL);
+                        $postFields = array_map(function($value) use($word) {
+                            return preg_replace('/\%word\%/', $word, $value);
+                        }, $this->postFields);
+                        curl_setopt_array($ch[$word], array(CURLOPT_RETURNTRANSFER => true, CURLOPT_POSTFIELDS => $postFields));
+                        curl_multi_add_handle($this->cURLHandler, $ch[$word]);
+                    }
+
+                    $running = null;
+                    do {
+                        curl_multi_exec($this->cURLHandler, $running);
+                    } while($running > 0);
+
+                    foreach($ch as $password => $handler) {
+                        $ch[$password] = $parse(curl_multi_getcontent($handler), $password);
+                        if($ch[$password]) {
+                            return $password;
+                        }
+                    }
+
+                    $ch = array();
+
+                    if($rounds > 0) {
+                        $i++;
+                        if($i >= $rounds) {
+                            $done = true;
+                        }
+                    }
+                    $h++;
+                }
+            }
+            return $ch;
+        } else {
+            if(!$this->permutation) {
+                foreach($this->wordList as $word) {
+                    $pattern = preg_replace('/\%word\%/', $word, $this->pattern);
+                    $hash = hash($this->hashType, $pattern);
+                    if($hash == $this->hash) {
+                        return $word;
                     }
                 }
-
-                $ch = array();
-
-                if($rounds > 0) {
-                    $i++;
-                    if($i >= $rounds) {
+            } else {
+                $h = 0;
+                $i = 0;
+                $done = false;
+                while($done == false) {
+                    if($i > $rounds) {
                         $done = true;
                     }
+
+                    $word = base_convert($h, 10, 36);
+                    $pattern = preg_replace('/\%word\%/', $word, $this->pattern);
+                    $hash = hash($this->hashType, $pattern);
+                    if($hash == $this->hash) {
+                        return $word;
+                    }
+                    $h++;
+
+                    if($rounds > 0) {
+                        $i++;
+                    }
                 }
-                $h++;
+
+                return false;
             }
         }
-        return $ch;
+    }
+
+    /**
+     *
+     * Load a set of finger prints (hash regexps).
+     *
+     * @param string $filename The fingerprint file.
+     *
+     * @return bool
+     *
+     */
+    public function loadFingerPrints($filename) {
+        if(!is_file($filename)) {
+            throw new \Exception(__METHOD__ . ': ' . $filename . ' does not exist.');
+        }
+
+        if($this->fingerprints = json_decode(file_get_contents($filename))) {
+            return true;
+        }
+
+        throw new \Exception(__METHOD__ . ': ' . $filename . ' contains malformed JSON.');
+    }
+
+    /**
+     *
+     * Set the hash to bruteforce.
+     *
+     * @param string $hash The hash to bruteforce.
+     *
+     * @return string
+     *
+     */
+    public function setHash($hash, $type = null, $pattern = '%word%') {
+        $this->hash = $hash;
+        $this->pattern = $pattern;
+        if($type !== null) {
+            if(!in_array($type, hash_algos())) {
+                throw new \Exception(__METHOD__ . ': Unknown hash method "' . $type . '"');
+            }
+            $this->hashType = $type;
+        }
+
+
+        foreach($this->fingerprints as $hashType => $fingerprint) {
+            if(preg_match('/' . $fingerprint . '/', $hash)) {
+                return $hashType;
+            }
+        }
     }
 
     /**
--- run.php
+++ run.php
@@ -5,10 +5,15 @@ $postFields = array(
 	'password' => '%word%'
 );
 $prutus = new Orpheus\Prutus('http://localhost/prutus2/prutus-test-interface.php', $postFields, __DIR__ . '/wordlist.txt');
+
 $prutus->setPermutation(true);
 $prutus->setBuffer(5);
+$prutus->setHash('c0c7c76d30bd3dcaefc96f40275bdc0a', 'md5', '%word%');
+
 $results = $prutus->start(function($data, $password) {
 	echo $data . PHP_EOL;
 	$data = json_decode($data);
 	return $data->success == 'logged_in' ? true : false;
 });
+
+var_dump($results);
